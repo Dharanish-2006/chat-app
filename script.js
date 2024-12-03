0089d428bac361cdef583435bb608c0f183738f7
Dharanish-2006 <dharanishwar.2006@gmail.com>
2024-12-03 10:14:32 +0530

    changes regarding username

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('What is your name?')
+const username = document.getElementById('user-name')
+const name = username.value
 appendMessage('You joined')
 socket.emit('new-user', name)
 
